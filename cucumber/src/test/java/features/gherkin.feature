b24a0af511b32777321461b96607866d0c8f488a
PabloAndresArg <PabloAndresArg@gmail.com>
2021-05-28 01:30:12 -0600

    10 TEST TERMINADOS

--- cucumber/src/test/java/features/gherkin.feature
+++ cucumber/src/test/java/features/gherkin.feature
@@ -69,15 +69,96 @@ Then observara la informacion en el ultimo mensaje de la contraseña que se asig
 
 
 
-@seleniumTest1
-Scenario Outline: Search for items and then move to checkout page
-Given User is on Greencart Langing page2
-When User seached for <vegetal> vegetable2
-And Added items to cart
-And User proceeded to Checkout page for purchase
-Then verify selected <vegetal> items are displayed in check	out page
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@escenario7
+Scenario Outline: Crear una Persona
+Given carga el panel administrativo de bdg2 "root@7arg"
+When hace click en desplegar menu
+And hace click en administracion 
+And selecciona  persona la cual se encuentra en el apartado de catalogos basicos
+And hace click en crear dentro del modulo de catalogos basicos persona
+And llena el formulario de creacion de persona con los campos nombres <nombres> , apellidos <apellidos>  y sexo <sexo> cumpliendo asi los campos necesarios
+Then hace click en guardar
+And Aparezca en la consulta de personas con su nombre <nombres> y apellidos <apellidos>
 Examples: 
-|vegetal  |
-|Cucumber |
-|Brinjal  |
+|nombres     |apellidos     |sexo|
+|t1          |t1            |M |
+
+ 
+
+ 
+@escenario8
+Scenario Outline: Convertir una persona en Empleado
+Given carga el panel administrativo de bdg2 "root@7arg"
+When hace click en desplegar menu
+And hace click en administracion 
+And selecciona  persona la cual se encuentra en el apartado de catalogos basicos
+And busca a la persona de nombres <nombres> y apellidos <apellidos> llenando el formulario dandole click en buscar 
+Then lo convierte en empleado 
+And verifica si se convirtio en empleado mediante una busqueda por nombre <nombres> y apellido <apellidos>
+Examples: 
+|nombres     |apellidos     |
+|t1          |t1            |
+
+
+
+@escenario9
+Scenario Outline: crear un usuario
+Given carga el panel administrativo de bdg2 "root@7arg"
+When hace click en desplegar menu
+And hace click en administracion 
+And hace click en configuracion de usuarios
+And hace click en administracion de usuarios
+And click en el boton para agrgar usuarios
+And busca al empleado por medio de nombre <nombres> y apellido <apellidos>
+And ingresa un nuevo usuario <username> ingresandolo en el textbox seguido presiona registar
+And se mueve a desplegar menu en la esquina derecha   Customer care mensajeria
+Then observara la informacion en el ultimo mensaje de la contraseña que se asigno
+Examples: 
+|nombres     |apellidos | username |
+|t1          |t1        |	t1user   |
+
+@escenario10
+Scenario Outline: asignacion de Rol
+Given carga el panel administrativo de bdg2 "root@7arg"
+When hace click en desplegar menu
+And hace click en administracion 
+And hace click en configuracion de usuarios
+And hace click en administracion de roles
+And hace click en asignar usuarios
+And busco el usuario por nombre <nombres> y apellido <apellidos> y su vez se selecciona
+Then verifica que se pueda logear con el usuario <username> y su password <password>
+And establecer una contraNueva <newPassword> quitando la antigua <password>
+And logeo con user <username> y pass <contraNueva> para verificar la nueva contrasena 
+Examples: 
+|nombres     |apellidos | username | password  | newPassword |
+|t1          |t1        |	t1user   |	t30ghxuo | Root@7arg   | 
+
+
--- cucumber/src/test/java/stepDefinations/Definiciones.java
+++ cucumber/src/test/java/stepDefinations/Definiciones.java
@@ -1,6 +1,8 @@
 package stepDefinations;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -232,7 +234,30 @@ public class Definiciones extends Base {
         driver.findElement(By.id("WucLogin_TxtClave")).sendKeys("Root@7arg");
   	  	driver.findElement(By.id("WucLogin_BtnLogin")).click();
 
-    	Thread.sleep(2000);
+    	Thread.sleep(1200);
+    	
+		driver.findElement( By.xpath("//select[@id='WucRolxUsuario_ddlRoles']" )).click();
+		 
+		 List<WebElement> lista_valores_combobox =   driver.findElement( By.xpath("//select[@id='WucRolxUsuario_ddlRoles']" )).findElements(By.xpath("option")); 
+		 Boolean testOK = false; 
+		 for(WebElement we : lista_valores_combobox) {
+			 
+			 if(we.getText().equalsIgnoreCase("administrador")) {
+			 testOK = true;
+			 Thread.sleep(500);
+			 we.click();
+			 if (we.isSelected()) {
+				 System.out.println("OK");
+					 break;
+				 }
+			 }
+			 
+		 }
+		 
+		 if(!testOK) {
+				Assert.assertFalse(true);
+		 }
+		 	Thread.sleep(2000);
   	  	driver.findElement(By.id("WucRolxUsuario_app_1")).click();
     }
     
@@ -260,7 +285,7 @@ public class Definiciones extends Base {
     
     @And("^se mueve a desplegar menu en la esquina derecha   Customer care mensajeria$")
     public void se_mueve_a_desplegar_menu_en_la_esquina_derecha_customer_care_mensajeria() throws Throwable {
-    	Thread.sleep(1350);
+    	Thread.sleep(1500);
 	  	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[1]/button[1]")).click();
 	  	Thread.sleep(1000);
 	  	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[2]/ul[1]/li[3]/a[1]/span[1]")).click();
@@ -272,46 +297,303 @@ public class Definiciones extends Base {
     @Then("^observara la informacion en el ultimo mensaje de la contraseña que se asigno$")
     public void observara_la_informacion_en_el_ultimo_mensaje_de_la_contrasea_que_se_asigno() throws Throwable {
 		driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/a[1]")).click();
-	  	Thread.sleep(1000);	
     }
     
     
     //---------------------------------------------------------------
+    
+    // TEST 7 ----------------------------------------------------
+    
 
-    @Given("^User is on Greencart Langing page2$")
-    public void user_is_on_greencart_langing_page_() throws Throwable {
-    	driver = Base.getDriver();
+    @And("^selecciona  persona la cual se encuentra en el apartado de catalogos basicos$")
+    public void selecciona_persona_la_cual_se_encuentra_en_el_apartado_de_catalogos_basicos() throws Throwable {
+	  	Thread.sleep(1000);
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[2]/ul[1]/li[1]/ul[1]/li[1]/a[1]")).click();
+	  	Thread.sleep(1000);
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[2]/ul[1]/li[1]/ul[1]/li[1]/ul[1]/li[13]/a[1]")).click();	
+	  	Thread.sleep(1000);   				
+    }
+    
+    
+    @And("^hace click en crear dentro del modulo de catalogos basicos persona$")
+    public void hace_click_en_crear_dentro_del_modulo_de_catalogos_basicos_persona() throws Throwable {
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[4]/div[1]/a[2]/span[1]")).click();
+      	Thread.sleep(1000);
+	  	
+    }
+    
+    @And("^llena el formulario de creacion de persona con los campos nombres (.+) , apellidos (.+)  y sexo (.+) cumpliendo asi los campos necesarios$")
+    public void llena_el_formulario_de_creacion_de_persona_con_los_campos_nombres_something_apellidos_something_y_sexo_something_cumpliendo_asi_los_campos_necesarios(String nombres, String apellidos, String sexo) throws Throwable {
+    	
+    Thread.sleep(800); 
+    driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[1]/div[2]/input[1]")).sendKeys(nombres);
+ 	Thread.sleep(800); 
+    driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[3]/div[2]/input[1]")).sendKeys(apellidos);
+ 	Thread.sleep(800); 
+ 	String xPATH = "";
+ 	if (sexo.equalsIgnoreCase("F")) {
+ 	  xPATH = "/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[5]/div[2]/table[1]/tbody[1]/tr[1]/td[1]/input[1]";
+ 	}else {
+ 	  xPATH = "/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[5]/div[2]/table[1]/tbody[1]/tr[1]/td[2]/input[1]";	
+ 	}
+    driver.findElement(By.xpath(xPATH)).click();
+ 	Thread.sleep(800); 
+
+    }
+
+    @Then("^hace click en guardar$")
+    public void hace_click_en_crear() throws Throwable {
+        System.out.println("CREA A UNA PERSONA");
+       
+        driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[16]/div[1]/a[2]/span[1]")).click();
+     	Thread.sleep(7000); 
+        	
+    }
+    
+    @And("^Aparezca en la consulta de personas con su nombre (.+) y apellidos (.+)$")
+    public void aparezca_en_la_consulta_de_personas_con_su_nombre_something_y_apellidos_something(String nombres, String apellidos) throws Throwable {
+    	Thread.sleep(800); 
+    	String xPATH = "/html[1]/body[1]/form[1]/nav[1]/div[1]/div[3]/div[1]/div[3]/a[1]";
+    	driver.findElement(By.xpath(xPATH)).click();
+    	Thread.sleep(1000); 
+    	
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[6]/div[2]/div[1]/input[1]")).sendKeys(nombres);
+    	Thread.sleep(1000); 	
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[6]/div[4]/div[1]/input[1]")).sendKeys(apellidos);
+     	Thread.sleep(1000); 
+     	
+     	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/a[1]")).click();
+     	
+     	Thread.sleep(5000); 
+    	String nombresApellidos =  driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/a[1]/span[1]")).getText();
+    	System.out.println("BUSCANDO:"+ nombres +" "+apellidos);
+    	System.out.println("Encuentra:"+nombresApellidos);
+    	if(!(nombresApellidos.equalsIgnoreCase(nombres +" "+apellidos))) {
+    		Assert.assertFalse(true);
+        	System.out.println("DIFERENTES!!");
+    	}else {
+    		System.out.println("OK SI LO ENCUENTRA");
+    	}
+    	 
     }
 
     
-    @When("^User seached for (.+) vegetable2$")
-    public void user_seached_for_vegetable2(String vegetal) throws Throwable {
+
+    
+    
+
+
+    //------------------------------------------------------------
+    
+    //TEST8------------------------------------------------------
+    @And("^busca a la persona de nombres (.+) y apellidos (.+) llenando el formulario dandole click en buscar$")
+    public void busca_a_la_persona_de_nombres_something_y_apellidos_something_llenando_el_formulario_dandole_click_en_buscar(String nombres, String apellidos) throws Throwable {
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[2]/div[2]/div[1]/input[1]")).sendKeys(nombres);
+    	Thread.sleep(1000); 	
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[2]/div[4]/div[1]/input[1]")).sendKeys(apellidos);
+     	Thread.sleep(1000); 
+     	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[4]/div[1]/a[1]")).click();
+
     	
-      	 h = new HomePage(driver);
-       	// h.getSearch().sendKeys(vegetal);
-       	// Thread.sleep(3000);
-    }
-
-    @Then("^verify selected (.+) items are displayed in check	out page$")
-    public void verify_selected_something_items_are_displayed_in_checkout_page(String strArg1) throws Throwable {
-    	//Assert.assertTrue( driver.findElement(By.cssSelector("p.product-name")).getText().contains(strArg1));
-    	System.out.println("ok");
-    }
-
-    @And("^Added items to cart$")
-    public void added_items_to_cart() throws Throwable {
-    	//driver.findElement(By.cssSelector("a.increment")).click();
-    	//sale analizando con  chrome
-    	//driver.findElement(By.xpath("//button[contains(text(),'ADD TO CART')]")).click();
-      	System.out.println("ok");
     	
     }
-
-    @And("^User proceeded to Checkout page for purchase$")
-    public void user_proceeded_to_checkout_page_for_purchase() throws Throwable {
-     	System.out.println("ok");
-    	//driver.findElement(By.xpath("//a[@class='cart-icon']//img[contains(@class,'')]")).click();
-    	//driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+    
+    
+ 
+    @Then("^lo convierte en empleado$")
+    public void lo_convierte_en_empleado() throws Throwable {
+    	Thread.sleep(5000); 
+    	driver.findElement(By.xpath(" /html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[6]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr[1]/td[7]/a[1]/span[1]")).click();
+    	Thread.sleep(5000); 
     }
+    
+    @And("^verifica si se convirtio en empleado mediante una busqueda por nombre (.+) y apellido (.+)$")
+    public void verifica_si_se_convirtio_en_empleado_mediante_una_busqueda_por_nombre_something_y_apellido_something(String nombres, String apellidos) throws Throwable {
+    	Thread.sleep(1000);
+	  	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[1]/button[1]")).click();
+	  	Thread.sleep(1000);
+	  	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[2]/ul[1]/li[1]/a[1]")).click();
+	 	Thread.sleep(1000);
+	 	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[2]/ul[1]/li[1]/a[1]")).click();
+	  	Thread.sleep(1000);
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[2]/ul[1]/li[1]/ul[1]/li[1]/a[1]")).click();
+	  	Thread.sleep(1000);
+	  	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[2]/ul[1]/li[1]/ul[1]/li[1]/ul[1]/li[5]/a[1]")).click();
+    	Thread.sleep(5000); 
+    
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div[4]/div[1]/input[1]")).sendKeys(nombres);
+    	Thread.sleep(1000); 	
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div[6]/div[1]/input[1]")).sendKeys(apellidos);
+     	Thread.sleep(1000); 
+     	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div[10]/div[1]/button[1]/i[1]")).click();
+      	Thread.sleep(3000); 
+      	System.out.println(driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div[11]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr[1]/td[2]/span[1]")));
+     	
+    	
+    }
+
+   
+
+    
+
+    //-----------------------------------------------------------
+    
+    
+    // TEST 9 -----------------------------------------------
+    
+
+    @And("^click en el boton para agrgar usuarios$")
+    public void click_en_el_boton_para_agrgar_usuarios() throws Throwable {
+    	Thread.sleep(1000); 
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/a[1]")).click();
+    }
+    
+
+    @And("^busca al empleado por medio de nombre (.+) y apellido (.+)$")
+    public void busca_al_empleado_por_medio_de_nombre_something_y_apellido_something(String nombres, String apellidos) throws Throwable {
+    	
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[2]/div[1]/input[1]")).sendKeys(nombres);
+     	Thread.sleep(1000); 
+     	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[2]/div[2]/input[1]")).sendKeys(apellidos);
+    	Thread.sleep(1000); 	
+     	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[6]/div[1]/div[1]/div[2]/div[3]/button[1]")).click();
+     	// CLICK ASIGNAR
+     	Thread.sleep(1000);
+     	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[7]/div[1]/div[1]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr[1]/td[4]/a[1]/span[1]")).click();    	
+    }
+    
+    @And("^ingresa un nuevo usuario (.+) ingresandolo en el textbox seguido presiona registar$")
+    public void ingresa_un_nuevo_usuario_something_ingresandolo_en_el_textbox_seguido_presiona_registar(String username) throws Throwable {
+    	Thread.sleep(1000);
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[2]/input[1]")).sendKeys(username);			
+    	Thread.sleep(1500);	
+    	System.out.println("USUARIO CREADO");
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]/input[1]")).click();    	
+    	Thread.sleep(6000);
+    }
+    
+
+    
+    //-------------------------------------------------------
+    
+    // TEST10 ----------------------------------------------
+    
+    @And("^hace click en administracion de roles$")
+    public void hace_click_en_administracion_de_roles() throws Throwable {
+    	Thread.sleep(1000);
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[2]/ul[1]/li[1]/ul[1]/li[2]/ul[1]/li[1]/a[1]")).click();			
+    }
+    
+    @And("^hace click en asignar usuarios$")
+    public void hace_click_en_asignar_usuarios() throws Throwable {
+    	Thread.sleep(1000);
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr[1]/td[5]/a[1]/span[1]")).click();			
+    	Thread.sleep(1000);
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/a[2]")).click();			
+    	
+    }
+    
+    @And("^busco el usuario por nombre (.+) y apellido (.+) y su vez se selecciona$")
+    public void busco_el_usuario_something_y_su_vez_se_selecciona(String nom , String apellidos) throws Throwable { // REPORTAR PROBLEMA CON USUARIO
+    	driver.manage().window().maximize();
+    	Thread.sleep(2000);  
+    	// input usersname   
+
+    	WebElement frame_modal = driver.findElement(By.xpath("//body/div[@id='s7modal']/div[1]/div[1]/div[2]/div[1]/div[1]/iframe[1]"));
+    	driver.switchTo().frame(frame_modal); // me muevo hacia ese marco
+    	Thread.sleep(1000);  
+    	driver.findElement(By.cssSelector("#ContentPlaceHolder_WucFiltroUsuarios_TxtNombrePersona")).sendKeys(nom);
+    	Thread.sleep(1000);  
+    	driver.findElement(By.cssSelector("#ContentPlaceHolder_WucFiltroUsuarios_TxtApellidoPersona")).sendKeys(apellidos);
+    	Thread.sleep(1000); 
+        // boton buscar
+    	
+    	driver.findElement(By.cssSelector("#ContentPlaceHolder_WucFiltroUsuarios_LnkBuscar")).click();	
+     	
+    	
+    	Thread.sleep(5000); 
+     	//---------------------------------------------------------------------------------------------------------------- 
+    	//CHECK
+    	
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[4]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/table[1]/tbody[1]/tr[1]/td[1]/input[1]")).click();
+     
+    	// BTN ASIGNAR ROL
+    	Thread.sleep(1500); 
+    	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/div[3]/div[2]/div[3]/div[1]/button[2]")).click();
+    	Thread.sleep(5000); 
+    	
+    	
+    	//SALIR---------------------------------
+    	
+    	driver.switchTo().parentFrame(); //regresa al frame anterior
+    	driver.findElement(By.xpath("//button[@id='btnModalClose']")).click();
+    	Thread.sleep(1200); 
+    	
+
+    	
+     	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[1]/button[1]")).click();
+    	Thread.sleep(1200); 
+    	// btn de salir
+     	driver.findElement(By.xpath("/html[1]/body[1]/form[1]/nav[1]/div[1]/div[2]/ul[1]/li[5]/a[1]/span[2]")).click();
+    	Thread.sleep(1200); 
+    	// confirma la salida
+     	driver.findElement(By.xpath("/html[1]/body[1]/div[5]/div[1]/div[1]/div[3]/div[1]/div[1]/button[2]")).click();
+    	Thread.sleep(1200); 
+    	
+    }
+    
+    @Then("^verifica que se pueda logear con el usuario (.+) y su password (.+)$")
+    public void verifica_que_se_pueda_logear_con_el_usuario_y_su_password(String username, String password) throws Throwable {
+    	Thread.sleep(600); 
+        driver.findElement(By.id("WucLogin_TxtUsuario")).sendKeys(username);
+        driver.findElement(By.id("WucLogin_TxtClave")).sendKeys(password);
+  	  	driver.findElement(By.id("WucLogin_BtnLogin")).click();
+    	Thread.sleep(2100);
+
+    	// sale que cambie la contraseña 
+    }
+    
+    @And("^establecer una contraNueva (.+) quitando la antigua (.+)$")
+    public void establecer_una_contranueva_quitando_la_antigua(String newpassword, String password) throws Throwable {
+
+    	Thread.sleep(1000);
+    	driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/form[1]/div[3]/div[1]/input[1]")).sendKeys(password);
+    	Thread.sleep(600);
+    	driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/form[1]/div[4]/div[1]/input[1]")).sendKeys(newpassword);
+    	Thread.sleep(400);
+    	driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/form[1]/div[5]/div[1]/input[1]")).sendKeys(newpassword);
+    	Thread.sleep(1200);
+
+    	driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/form[1]/div[6]/div[2]/input[1]")).click();
+      	Thread.sleep(5000);
+
+      	// cierra sesion
+
+      	
+      	System.out.print("ok");
+    }
+    
+    @And("^logeo con user (.+) y pass (.+) para verificar la nueva contrasena$")
+    
+    public void logeo_con_user_y_pass_para_verificar_la_nueva_contrasena(String username, String contranueva) throws Throwable {
+    	Thread.sleep(2000);
+        driver.findElement(By.id("WucLogin_TxtUsuario")).sendKeys(username);
+        driver.findElement(By.id("WucLogin_TxtClave")).sendKeys(contranueva);
+        
+        driver.findElement(By.id("WucLogin_BtnLogin")).click();
+      	Thread.sleep(2000);
+        String texto =  driver.findElement(By.id("WucRolxUsuario_lbSaludo")).getText();
+        
+    }
+
+
+    
+ 
+    		
+    
+    		
+    	
+    
+    //------------------------------------------------------
 
 }
